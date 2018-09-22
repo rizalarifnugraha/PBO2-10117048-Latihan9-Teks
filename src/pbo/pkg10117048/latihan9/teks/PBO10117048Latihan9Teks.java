a320fea369e3cfddfc5e97f06a2dc60bc3ec64f0
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 19:53:37 +0700

    Add files via upload

---
+++ src/pbo/pkg10117048/latihan9/teks/PBO10117048Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10117048.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program menggunakan char.
+ */
+public class PBO10117048Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       char c;
+       char tab = '\t';
+       char newline = '\n';
+       c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c );
+       
+    }
+    
+}
